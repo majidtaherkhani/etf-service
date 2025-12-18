f713a99277e4e33bd33c1255b00b82cfc53a59db
Majid Taherkhani <majidtaherkhani555@gmail.com>
2025-12-17 19:46:32 -0500

    Add CORS middleware configuration

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from slowapi.middleware import SlowAPIMiddleware
 from slowapi import _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
@@ -6,6 +7,19 @@ from src.modules.etf.router import router as etf_router
 from configs.limiter import limiter
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://etf-web-wine.vercel.app",
+        "http://localhost:3000",
+        "http://localhost:5173", 
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
