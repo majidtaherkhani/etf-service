7b97e4f7ce3607777de06f02b70cf929b7ba14b2
Majid Taherkhani <majidtaherkhani555@gmail.com>
2025-12-19 10:27:17 -0500

    fix: enable pool_pre_ping to prevent database connection errors

--- configs/db/postgresql.py
+++ configs/db/postgresql.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_POSTGRESQL_URL")
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
